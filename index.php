52f17e687b0dfc91149dd0b520064c31828f705f
chris911 <christopherchude19@gmail.com>
2024-07-08 11:29:08 +0100

    my first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>class</title>
+    <link rel="stylesheet" href="asset/css/style.css">
+</head>
+<body>
+    <nav>
+
+    </nav>
+    <nav>
+        <div>
+            <p>registration</p>
+        </div>
+        <ul>
+            <li>registrer</li>
+            <li>register</li>
+        </ul>
+    </nav>
+    <form action="includes/_partials/save.php" methods="post">
+        <label for="first name">first name </label>
+        <input type="text" name="first_name"  id="">
+        <label for="last name">last name</label>
+        <input type="text" name ="last name" id="">
+        <label for="email">email</label>
+        <input type="email" name="email id="">
+        <label for="age">age</label>
+        <input type="number" name="age" id="" min="1">
+        <input type="submit" value="save student">
+</body>
+</html>
